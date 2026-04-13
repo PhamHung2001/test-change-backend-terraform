dc7e7d197c2901ca2827da4144d472cb81c66ed9
PhamHung2001 <64305660+PhamHung2001@users.noreply.github.com>
2026-04-13 23:22:59 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
    ami           = "ami-0e7ff22101b84bcff"
    instance_type = "t2.micro"
 
-   vpc_security_group_ids = ["sg-02d2f7dd78a77231f"]
+   vpc_security_group_ids = ["sg-0d069d67be439de67"]
 
    tags = {
      Name = "bastion"
