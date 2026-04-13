7c104dabc7a10a6f7cef345ba4cda7de71829a9f
PhamHung2001 <64305660+PhamHung2001@users.noreply.github.com>
2026-04-13 23:19:45 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  resource "aws_instance" "example" {
-   ami           = "ami-024e6efaf93d85776"
+   ami           = "ami-0e7ff22101b84bcff"
    instance_type = "t2.micro"
 
    vpc_security_group_ids = ["sg-02d2f7dd78a77231f"]
